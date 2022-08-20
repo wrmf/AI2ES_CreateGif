5804fcc8c51687776554fe6187ad439a6cb3b4c6
wrmf <31448920+wrmf@users.noreply.github.com>
2022-08-20 14:25:35 -0500

    updated file names

--- CreateGif.py
+++ CreateGif.py
@@ -5,7 +5,7 @@ import glob
 images = []
 
 path = 'images'
-for filename in sorted(glob.glob(os.path.join(path, '*.png'))):
+for filename in sorted(glob.glob(os.path.join(path, 'Page*.png'))):
     images.append(imageio.imread(filename))
 
 imageio.mimsave('images/alphaBeta.gif', images, duration=1)
